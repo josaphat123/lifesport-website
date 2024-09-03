9920cd3cfef5f5f5fcbfe1607addc4b49fee7233
JeanPhilippeOuraga <Jeanphilippeouraga52@gmail.com>
2024-09-03 01:49:44 +0000

    avancement sur la page index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -329,19 +329,18 @@
         <!--services section end-->
 
         <!--call to action section start-->
-        <section class="call-to-action ptb-100 gradient-overlay" style="background: url('assets/img/hero-bg-4.jpg')no-repeat center center / cover">
+        <section class="call-to-action ptb-100 gradient-overly" style="background: url('lifesport/assets/img/img-fond/img-2.jpg')no-repeat center center / cover">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-10 col-lg-9">
                         <div class="call-to-action-content text-white text-center">
-                            <h2 class="text-white">Need Some Help?</h2>
-                            <p class="lead mb-0">Whether you’re stuck or just want some tips on where to start, hit up our
-                                experts anytime.</p>
-                            <p>Sales Chat: <strong> MON-FRI 8AM-4PM PT</strong> | Customer Support Chat: Every Day <strong>
-                                5:30AM–9:30PM PT</strong></p>
+                            <h2 class="text-white">Profitez d'une Consultation Gratuite !</h2>
+                            <p class="lead mb-0">L'occasion <strong>unique</strong> pour vous de reprendre le contrôle grâce à notre Expertise.</p>
+                            <p>Horaire : <strong> Lun-Ven 8h00-19h00 PT</strong> | Support Client : Tous les jours <strong>
+                                24h/24 7jours/7</strong></p>
                             <div class="action-btns">
-                                <a href="#" class="btn solid-white-btn mr-3">Chat With Us</a>
-                                <a href="#" class="btn outline-white-btn">Send us an Email</a>
+                                <a target="_BLANK" href="https://wa.me/message/JWAKAZV73SWIN1" class="btn primary-solid-btn mr-3">Discuter avec-nous</a>
+                                <a href="#" class="btn outline-white-btn">Envoyer un E-Mail</a>
                             </div>
                         </div>
                     </div>
